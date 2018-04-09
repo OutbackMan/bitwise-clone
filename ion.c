0fa64f096f524dea862b9b2052a95a61d229a3a9
Ryan <re.mcclue@gmail.com>
2018-04-09 18:20:11 +1000

    start string interning

--- ion.c
+++ ion.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdint.h>
+#include <ctype.h>
 
 #ifdef _MSC_VER
 #define _CRT_SECURE_NO_WARNINGS // This removes warnings for not using '_s' variants
@@ -44,7 +46,7 @@ typedef struct {
 	(BUF__FIT(b, 1), b[BUF_LENGTH(b)] = (element), BUF__HEADER(b)->length++) 
 
 #define BUF_FREE(b)	\
-	(((b) != NULL) ? free(BUF__HEADER(b)) : 0)
+	(((b) != NULL) ? free(BUF__HEADER(b)) : 0, (b) = NULL)
 
 void* buf__grow(const void* buf, size_t new_length, size_t elem_size)
 {
@@ -62,25 +64,51 @@ void* buf__grow(const void* buf, size_t new_length, size_t elem_size)
 	}
 
 	new_header->capacity = new_cap;
+
 	return new_header->buffer; 
 }
 
-/*
+typedef struct {
+	size_t length;	
+	const char* str;
+} InternStr;
+
+InternStr* interns;
+
+const char* str_intern_range(const char* start, const char* end)
+{
+	size_t length = end - start;	
+	for (size_t i = 0; i < BUF_LENGTH(interns); ++i) {
+		if (interns[i].length == length && strncmp(interns[i].str, start, length) == 0) {
+			return interns[i].str;	
+		}
+	}
+	char* str = malloc(length + 1);
+	memcpy(str, start, length);
+	str[length] = '\0';
+	BUF_PUSH(interns, ((InternStr){length, str})); // pre-processor requires these to be wrapped in parentheses to avoid parameter delimiting
+}
+
+const char* str_intern(const char* str)
+{
+	return str_intern_range(str, str + strlen(str));	
+}
+
 // Lexing
 typedef enum {
-	TOKEN_INT = 128,
+	// reserve first 128 values for one char tokens
+	TOKEN_LAST_CHAR = 127,
+	TOKEN_INT,
 	TOKEN_NAME
 	// ..
 } TokenType;
 
 typedef struct {
 	TokenType type; 	
+	const char* start;
+	const char* end;
 	union {
 		uint64_t val;		
-		struct {
-			const char* start;
-			const char* end;
-		};
 	}
 } Token;
 
@@ -89,38 +117,96 @@ const char* stream;
 
 void next_token(void)
 {
+	token.start = stream;
 	switch (*stream) {
-	 case 0 ... 9:
+	 case 0:
+	 case 1:
+	 case 2:
+	 case 3:
+	 case 4:
+	 case 5:
+	 case 6:
+	 case 7:
+	 case 8:
+	 case 9:
 	 {
 		uint64_t val = 0;
 		while (isdigit(*stream)) {
 			val *= 10;
 			val += *stream++ - '0'; // convert ascii to number
-			stream++;		
 		}
 		token.type = TOKEN_INT;
 		token.val = val;
 	 } break;
-	 case 'a' ... 'z':
-	 case 'A' ... 'Z':
+	 case 'a':
+	 case 'b':
+	 case 'c':
+	 case 'd':
+	 case 'e':
+	 case 'f':
+	 case 'g':
+	 case 'h':
+	 case 'i':
+	 case 'j':
+	 case 'k':
+	 case 'l':
+	 case 'm':
+	 case 'n':
+	 case 'o':
+	 case 'p':
+	 case 'q':
+	 case 'r':
+	 case 's':
+	 case 't':
+	 case 'u':
+	 case 'v':
+	 case 'w':
+	 case 'x':
+	 case 'y':
+	 case 'z':
+	 case 'A':
+	 case 'B':
+	 case 'C':
+	 case 'D':
+	 case 'E':
+	 case 'F':
+	 case 'G':
+	 case 'H':
+	 case 'I':
+	 case 'J':
+	 case 'K':
+	 case 'L':
+	 case 'M':
+	 case 'N':
+	 case 'O':
+	 case 'P':
+	 case 'Q':
+	 case 'R':
+	 case 'S':
+	 case 'T':
+	 case 'U':
+	 case 'V':
+	 case 'W':
+	 case 'X':
+	 case 'Y':
+	 case 'Z':
 	 case '_':
 	 {
-		const char* start = stream++;
 		while (isalnum(*stream) || *stream == '_') {
 			stream++;		
 		}
 		token.type = TOKEN_NAME;
-		token.start = start;
-		token.end = stream;
 	 } break;
 	 default:
+	 {
 		token.type = *stream++;
+	 } break;
 	}	
+	token.end = stream;
 }
 
 // printf("%.*s", str_length, str); useful for non-null terminated strings
 // casting to avoid compiler warnings is a bad habit
-*/
 
 int main(void)
 {
